1db6a669d0a160629cbb4060a7d8e68785d66a56
bradthomerson-web <bradthomerson@gmail.com>
2025-12-03 14:28:26 -0500

    added swing 911

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import SwingmasterAi from './SwingmasterAi'; // match actual filename casing
+import SwingmasterAi from './SwingmasterAi';
 import './index.css';
-import EmergencyTips from './emergencyTips';
+import EmergencyTips from './EmergencyTips'; // Ensure this matches your file name (Capital E usually)
 
 function App() {
-    return <SwingmasterAi />;
+    // Toggle this to false if you want to test the "Locked" view!
+    const isPro = true; 
+
+    return (
+        <div className="app-container">
+            {/* Your Main App */}
+            <SwingmasterAi />
+            
+            {/* Your New Emergency Feature */}
+            <div style={{ marginTop: '20px', padding: '20px' }}>
+                <EmergencyTips isProUser={isPro} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
